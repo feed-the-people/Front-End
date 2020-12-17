e6bd42a11330ea411ade431d747fec39be15c9cc
Nathaniel Millard <nathanielmillard@gmail.com>
2020-12-16 17:32:11 -0700

    Update test to pass.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('basic title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText("Feed The People");
+  expect(title).toBeInTheDocument();
 });
