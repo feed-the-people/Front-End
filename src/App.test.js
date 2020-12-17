5ab58ba7e166d5e14bd601907a9803a63012a195
Nathaniel Millard <nathanielmillard@gmail.com>
2020-12-17 15:48:48 -0700

    Move files into neccesary file structure.

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('basic title', () => {
-  render(<App />);
-  const title = screen.getByText("Feed The People");
-  expect(title).toBeInTheDocument();
-});
