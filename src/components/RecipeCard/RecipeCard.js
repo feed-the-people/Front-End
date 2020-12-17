4580e35c8d2d58c24ebed2b0c06d7bae79bec9ef
Nathaniel Millard <nathanielmillard@gmail.com>
2020-12-17 15:50:15 -0700

    Create basic render files for neccesary components.

---
+++ src/components/RecipeCard/RecipeCard.js
@@ -0,0 +1,13 @@
+
+
+function RecipeCard() {
+  return (
+    <div className="RecipeCard">
+      <header className="RecipeCard-header">
+        <h1> Recipe Card </h1>
+      </header>
+    </div>
+  );
+}
+
+export default RecipeCard;
---
+++ src/components/SignIn/SignInPage.js
@@ -0,0 +1,12 @@
+
+function SignInPage() {
+  return (
+    <div className="SignInPage">
+      <header className="SignInPage-header">
+        <h1> Sign In Page </h1>
+      </header>
+    </div>
+  );
+}
+
+export default SignInPage;
