434b2aecca8d5ff71d7a724af617fc5a7c40b4cc
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-04 13:50:59 -0700

    Create basic header buttons to naviate from this page.

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,11 +1,17 @@
 import './MainPage.css';
+import profile from '../../icons/user-icon.svg'
+import recipeBook from '../../icons/recipe-book-icon.svg'
+import {Link} from 'react-router-dom'
 
 function MainPage() {
+
   return (
     <div className="MainPage">
-      <header className="MainPage-header">
-        <h1> Main Page </h1>
+      <header className="MainPage-sidebar">
+        <Link to='/profile'><img src={profile} alt='navigate to user profile'/></Link>
+        <Link to='recipeBook'><img src={recipeBook} alt='navigate to user recipe book'/></Link>
       </header>
+
     </div>
   );
 }
