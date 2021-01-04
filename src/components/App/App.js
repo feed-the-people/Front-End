dbc06226b67c35bc2d2926276c3677f2ff4a6694
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-04 16:16:07 -0700

    Build basic route structure.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import MainPage from '../MainPage/MainPage'
+import SignInPage from '../SignIn/SignInPage'
+import SignUpPage from '../SignUp/SignUpPage'
+import RecipePage from '../RecipePage/RecipePage'
+
 import {
   BrowserRouter,
   Switch,
@@ -9,10 +13,29 @@ import {
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <h1>Feed The People </h1>
-      <MainPage />
-    </div>
+    <Switch>
+      <div className="App">
+        <h1>Feed The People </h1>
+        <Route path='/signin'>
+          <SignInPage />
+        </Route>
+        <Route path='/signup'>
+          <SignUpPage />
+        </Route>
+        <Route path='/recipepage'>
+          <RecipePage />
+        </Route>
+        <Route path='/profile'>
+
+        </Route>
+        <Route path='/recipebook'>
+
+        </Route>
+        <Route path='/'>
+          <MainPage />
+        </Route>
+      </div>
+    </Switch>
     </BrowserRouter>
   );
 }
