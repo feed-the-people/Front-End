1d669ca6d010d44c553c5a95d5b5364967b9e945
Brigette Doelp <brigette.doelp@gmail.com>
2021-01-05 22:21:39 -0700

    Build base JSX structure for RecipeCard

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -2,14 +2,33 @@ import './RecipeCard.css';
 import { allRecipes, individualUserData, allNPOs } from '../../mockData.js'
 import {Link} from 'react-router-dom'
 
-
+//alrighty so based on the wireframe of the recipe cards we're gonna
+//need a few more things other than just the recipe itself and the key.
+//just gonna slap that in hardcoded for the start!
 
 function RecipeCard() {
   return (
     <div className="RecipeCard">
-      <header className="RecipeCard-header">
-        <h1> Recipe Card </h1>
-      </header>
+      <section className='leftSection'>
+        <div className='leftHeader'>
+          <h1>Recipe Title</h1>
+          <h3>Rating</h3>
+        </div>
+        <img alt='recipeImage'>
+        <div className='leftFooter'>
+          <h3>NPO</h3>
+          <h3>Give N' Get Recipe!</h3>
+        </div>
+      </section>
+      <section className='rightSection'>
+        <h4>Tag Filter :</h4>
+        <section className='starRating'>
+          <img alt='theStarRatingImagesHere'>
+        </section>
+        <p className='recipeStory'></p>
+        <h4>Tags</h4>
+        <h5>#this #is #where #tags #go</h5>
+      </section>
     </div>
   );
 }
