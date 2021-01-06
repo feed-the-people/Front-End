057e3f0c18115c8de9f3e7f140b2c672ac35b2f5
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-06 11:45:37 -0700

    Add class based functionality to submit form.

--- src/components/SignIn/SignInPage.js
+++ src/components/SignIn/SignInPage.js
@@ -1,25 +1,45 @@
 import './SignInPage.css'
 import {Link} from 'react-router-dom'
+import { Component } from 'react';
 
-function SignInPage() {
-  return (
-    <div className="SignInPage">
-      <form>
-        <label>
-          email
-          <input type='email'/>
-        </label>
-        <label>
-          password
-          <input type='password'/>
-        </label>
-        <button> Submit </button>
-      </form>
-      <footer className="SignInPage-footer">
-        <Link to='/'><button> Take Me Back </button></Link>
-      </footer>
-    </div>
-  );
+class SignInPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+  updateInput = (e) => {
+    let type = e.target.type
+    let value = e.target.value
+    this.setState({[type]: value})
+  }
+
+  submitForm = () => {
+    console.log('Submitted!')
+  }
+
+  render() {
+    return (
+      <div className="SignInPage">
+        <form>
+          <label>
+            email
+            <input type='email' onChange={this.updateInput}/>
+          </label>
+          <label>
+            password
+            <input type='password' onChange={this.updateInput}/>
+          </label>
+          <button type='submit' onClick={this.submitForm}> Submit </button>
+        </form>
+        <footer className="SignInPage-footer">
+          <Link to='/'><button> Take Me Back </button></Link>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default SignInPage;
