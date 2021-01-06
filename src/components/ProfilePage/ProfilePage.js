0dcc3fe0bd74edbb7c7f32848b6a77c36cf35322
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-06 12:18:56 -0700

    Add button to head back if user doesn't want to signin/up.

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -1,22 +1,26 @@
 import Profile from '../Profile/Profile.js'
+import './ProfilePage.css'
 import {Link} from 'react-router-dom'
 
 const ProfilePage = (props) => {
-  const switchBoard = (
-      <section>
+  const callToAction = (
+      <section className ='callToAction'>
         <h1>Looks like you aren't sign in yet!</h1>
         <p> Sign in if you already have an acount with us,
         or signing up is easy. Get started sharing recipes and helping
         folks today! </p>
-        <Link to='/signin'> <button> Sign In </button> </Link>
-        <Link to='/signup'> <button> Sign Up </button> </Link>
+        <div className='buttons'>
+          <Link to='/signin'> <button> Sign In </button> </Link>
+          <Link to='/signup'> <button> Sign Up </button> </Link>
+        </div>
+        <footer className="callToAction-footer">
+          <Link to='/'><button> Take Me Back </button></Link>
+        </footer>
       </section>
     )
   return (
     <div className="ProfilePage">
-      <header className="ProfilePage-header">
-      </header>
-      {props.signedIn ? <Profile /> : switchBoard}
+      {props.signedIn ? <Profile /> : callToAction}
     </div>
   );
 }
