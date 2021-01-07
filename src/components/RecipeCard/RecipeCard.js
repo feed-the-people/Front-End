110fd61f29fcaeadbbbc9cebbb5ec5782d5424e0
Brigette Doelp <brigette.doelp@gmail.com>
2021-01-07 14:40:34 -0700

    Hardcode in mock data

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -2,30 +2,32 @@ import './RecipeCard.css';
 import { allRecipes, individualUserData, allNPOs } from '../../mockData.js'
 import {Link} from 'react-router-dom'
 
-//alrighty so based on the wireframe of the recipe cards we're gonna
-//need a few more things other than just the recipe itself and the key.
-//just gonna slap that in hardcoded for the start!
-
 function RecipeCard() {
+
+  let displayRecipeName = () => {
+
+  }
+
   return (
     <div className="RecipeCard">
       <section className='left-section'>
         <div className='leftHeader'>
           <h1 className='recipe-name'>{allRecipes[0].title}</h1>
-          <h3 className='recipe-rating'>4.5</h3>
+          <h3 className='recipe-rating'>5</h3>
         </div>
+        <img className='recipe-image' src={allRecipes[0].image} />
         <div className='left-footer'>
-          <h3 >NPO</h3>
-          <h3>Give N' Get Recipe!</h3>
+          <h3 className='nonprofit-name'>{allNPOs[0].name}</h3>
+          <Link to='/'><button className='purchase-button'>Give N' Get Recipe!</button></Link>
         </div>
       </section>
       <section className='right-section'>
         <h4>Tag Filter :</h4>
         <section className='star-rating'>
         </section>
-        <p className='recipe-story'></p>
+        <p className='recipe-story'>{allRecipes[0].description}</p>
         <h4 className='tags'>Tags</h4>
-        <h5 className='recipe-tags'>#this #is #where #tags #go</h5>
+        <h5 className='recipe-tags'></h5>
       </section>
     </div>
   );
