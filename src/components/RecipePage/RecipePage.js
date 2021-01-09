c8f0d841d14a34d4f3518b0c922c14c01a6c48fa
Carlyle Douglas <cdouglascorp@gmail.com>
2021-01-09 16:17:04 -0700

    Create return to display Recipe Page once recipe is clicked on Main Page

--- src/components/RecipePage/RecipePage.js
+++ src/components/RecipePage/RecipePage.js
@@ -1,11 +1,25 @@
 import './RecipePage.css';
+import {allRecipes} from '../../mockData.js'
 
 function RecipePage() {
+  let recipe1 = allRecipes[0]
+  console.log(recipe1)
+  // let recipeIngredients = recipe1.ingredients.map(ingredient => <p>{ingredient}</p>)
+
+
   return (
     <div className="RecipePage">
       <header className="RecipePage-header">
         <h1> Recipe Page </h1>
       </header>
+      <section className="recipe-section">
+        <h3>{recipe1.title}</h3>
+        <p><img src={recipe1.image} /></p>
+        <p>{recipe1.description}</p>
+        <p>{recipe1.instructions}</p>
+        {/* {recipeIngredients} */}
+      </section>
+
     </div>
   );
 }
