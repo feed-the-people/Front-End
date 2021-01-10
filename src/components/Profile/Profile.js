77a09f61a3e5e3b3b6fe3020425e05e48d1ff0e4
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-10 16:16:56 -0700

    Create basic profile rendering from locally stored info.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,10 +1,35 @@
+import './Profile.css'
 
 function Profile() {
+  let user = JSON.parse(localStorage.getItem('user'))
+  console.log(user)
   return (
     <div className="Profile">
-      <header className="Profile-header">
-      <h1>Profile</h1>
-      </header>
+      <h1>{`Welcome back ${user.firstName}! `}</h1>
+      <section className='profileInfo'>
+        <h2>Your Information: </h2>
+        <div className='whoYouAre'>
+          <label>
+            Your Name:
+            <p>{user.firstName} {user.lastName}</p>
+          </label>
+          <label>
+            Your Username:
+            <p>{user.username}</p>
+          </label>
+          <img src={user.image}/>
+        </div>
+        <label>
+          Your Email:
+          <p>{user.email}</p>
+        </label>
+        <label>
+          Your address:
+          <p>{user.street}</p>
+          <p>{user.city}, {user.state}</p>
+          <p>{user.zip}</p>
+        </label>
+      </section>
     </div>
   );
 }
