60b5e951f5ef7ed0cb333e1ccf0435c8600cdf92
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-10 16:29:59 -0700

    Add profile side bar.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,19 @@
 import './Profile.css'
+import {Link} from 'react-router-dom'
+import recipeBook from '../../icons/recipe-book-icon.svg'
+import globalIcon from '../../icons/global.svg'
 
 function Profile() {
   let user = JSON.parse(localStorage.getItem('user'))
   console.log(user)
   return (
     <div className="Profile">
-      <h1>{`Welcome back ${user.firstName}! `}</h1>
+      <header className="Profile-sidebar">
+        <Link to='/'><img src={globalIcon} alt='navigate to global feed'/></Link>
+        <Link to='/recipebook'><img src={recipeBook} alt='navigate to user recipe book'/></Link>
+      </header>
       <section className='profileInfo'>
+        <h1>{`Welcome back ${user.firstName}! `}</h1>
         <h2>Your Information: </h2>
         <div className='whoYouAre'>
           <label>
