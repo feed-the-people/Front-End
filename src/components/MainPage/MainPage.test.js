bf838afea19b6c1ea29c8fff5fe18140d4d0e530
Brigette Doelp <brigette.doelp@gmail.com>
2021-01-10 15:35:58 -0700

    Alter screen. targeting for test ids

--- src/components/MainPage/MainPage.test.js
+++ src/components/MainPage/MainPage.test.js
@@ -3,14 +3,14 @@ import '@testing-library/jest-dom'
 import { MemoryRouter } from 'react-router-dom'
 import MainPage from './MainPage.js';
 
-test('basic title', () => {
+test('main page renders', () => {
   render(
     <MemoryRouter>
       <MainPage />
     </MemoryRouter>
   );
-  const profileNav = screen.getByAltText("navigate to user profile page");
-  const recipeNav = screen.getByAltText("navigate to user recipe book");
+  const profileNav = screen.getByTestId("profileicon");
+  const recipeNav = screen.getByTestId("recipebookicon");
   expect(profileNav).toBeInTheDocument();
   expect(recipeNav).toBeInTheDocument();
 });
