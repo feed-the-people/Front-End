04a910db3f69e77cff5cc524cebfbba2b7037c8e
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-11 11:27:30 -0700

    Start refactor build.

--- src/components/RecipeBook/RecipeBook.js
+++ src/components/RecipeBook/RecipeBook.js
@@ -4,45 +4,47 @@ import home from '../../icons/global.svg'
 import {Link} from 'react-router-dom'
 import RecipeCard from '../RecipeCard/RecipeCard'
 import CallToAction from '../CallToAction/CallToAction.js'
-import { getUserWithRecipes, boughtRecipesByUser } from '../../APICalls.js'
+import { getUserWithRecipes } from '../../APICalls.js'
 
 let RecipeBook = (props) => {
   let user = JSON.parse(localStorage.getItem('user'))
   let userRecipes, userBoughtRecipes
-  let getUserInfo = async (id) => {
-    let userRecipes = await getUserWithRecipes(id)
-    let userBoughtRecipes = await boughtRecipesByUser(id)
-    if(userRecipes.recipes.length){
-      userRecipes.recipes.map((recipe, index) => {
-        return (
-          <RecipeCard
-            key={index}
-            image={recipe.image}
-            title={recipe.title}
-            charityName={recipe.charityName}
-            description={recipe.description}
-            />
-        )
-      })
-    }
-    if(userBoughtRecipes.length){
-      userBoughtRecipes.map((recipe, index) => {
-        return (
-          <RecipeCard
-            key={index}
-            image={recipe.image}
-            title={recipe.title}
-            charityName={recipe.charityName}
-            description={recipe.description}
-            />
-        )
-      })
-    }
-    return [userRecipes, userBoughtRecipes]
-  }
+  // let getUserInfo = async (id) => {
+  //   let userRecipes = await getUserWithRecipes(id)
+  //   let userBoughtRecipes = await boughtRecipesByUser(id)
+  //   if(userRecipes.recipes.length){
+  //     userRecipes.recipes.map((recipe, index) => {
+  //       return (
+  //         <RecipeCard
+  //           key={index}
+  //           image={recipe.image}
+  //           title={recipe.title}
+  //           charityName={recipe.charityName}
+  //           description={recipe.description}
+  //           />
+  //       )
+  //     })
+  //   }
+  //   if(userBoughtRecipes.length){
+  //     userBoughtRecipes.map((recipe, index) => {
+  //       return (
+  //         <RecipeCard
+  //           key={index}
+  //           image={recipe.image}
+  //           title={recipe.title}
+  //           charityName={recipe.charityName}
+  //           description={recipe.description}
+  //           />
+  //       )
+  //     })
+  //   }
+  //   return [userRecipes, userBoughtRecipes]
+  // }
+  let allRecipes;
+  // let allRecipes = getUserInfo(user.id)
+  // console.log(allRecipes[0])
 
-  let allRecipes = getUserInfo(user.id)
-  console.log(allRecipes[0])
+  getUserWithRecipes(user.id)
 
   if (!user) {
     return <CallToAction title='You need to be signed in to have a recipe book...' />
@@ -56,11 +58,11 @@ let RecipeBook = (props) => {
         <section className='my-recipe-section'>
           <h1>Recipes you have uploaded: </h1>
           <Link to='/recipeform'><button>Upload another recipe</button></Link>
-          {allRecipes[0] || <h4>You haven't uploaded any recipes</h4>}
+          {allRecipes || <h4>You haven't uploaded any recipes</h4>}
           </section>
           <section className='purchased-recipe-section'>
           <h1>Recipes you have purchased: </h1>
-          {allRecipes[1] || <h4>You haven't purchased any recipes</h4>}
+          {allRecipes || <h4>You haven't purchased any recipes</h4>}
         </section>
       </div>
     );
