a62715453f85e2774c51c3c8a9a105def4ab49c1
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-11 11:19:25 -0700

    Attempt to agrigate data.

--- src/components/RecipeBook/RecipeBook.js
+++ src/components/RecipeBook/RecipeBook.js
@@ -2,43 +2,66 @@ import './RecipeBook.css';
 import profile from '../../icons/user-icon.svg'
 import home from '../../icons/global.svg'
 import {Link} from 'react-router-dom'
-import {allRecipes} from '../../mockData.js'
 import RecipeCard from '../RecipeCard/RecipeCard'
 import CallToAction from '../CallToAction/CallToAction.js'
+import { getUserWithRecipes, boughtRecipesByUser } from '../../APICalls.js'
 
-function RecipeBook(props) {
-  //will ordinarily map over props being passed in, but in this case taking the direct import
-  let myUploadedRecipes, myPurchasedRecipes;
-  if (props.userRecipes){
-    myUploadedRecipes = allRecipes.map((recipe, index)=> {
-      return (
-        <RecipeCard recipe={recipe} key={index}/>
-      )
-    })
-    myPurchasedRecipes = allRecipes.map((recipe, index)=> {
-      return (
-        <RecipeCard recipe={recipe} key={index}/>
-      )
-    })
+let RecipeBook = (props) => {
+  let user = JSON.parse(localStorage.getItem('user'))
+  let userRecipes, userBoughtRecipes
+  let getUserInfo = async (id) => {
+    let userRecipes = await getUserWithRecipes(id)
+    let userBoughtRecipes = await boughtRecipesByUser(id)
+    if(userRecipes.recipes.length){
+      userRecipes.recipes.map((recipe, index) => {
+        return (
+          <RecipeCard
+            key={index}
+            image={recipe.image}
+            title={recipe.title}
+            charityName={recipe.charityName}
+            description={recipe.description}
+            />
+        )
+      })
+    }
+    if(userBoughtRecipes.length){
+      userBoughtRecipes.map((recipe, index) => {
+        return (
+          <RecipeCard
+            key={index}
+            image={recipe.image}
+            title={recipe.title}
+            charityName={recipe.charityName}
+            description={recipe.description}
+            />
+        )
+      })
+    }
+    return [userRecipes, userBoughtRecipes]
   }
-  if (!props.loggedIn) {
+
+  let allRecipes = getUserInfo(user.id)
+  console.log(allRecipes[0])
+
+  if (!user) {
     return <CallToAction title='You need to be signed in to have a recipe book...' />
   } else {
     return (
       <div className="RecipeBook">
-      <header className="RecipeBook-sidebar">
-      <Link to='/profilepage'><img src={profile} alt='navigate to user profile page'/></Link>
-      <Link to='/'><img src={home} alt='navigate to main page'/></Link>
-      </header>
-      <section className='my-recipe-section'>
-      <h1>Recipes you have uploaded: </h1>
-      <Link to='/recipeform'><button>Upload another recipe</button></Link>
-      {myUploadedRecipes || <h4>You haven't uploaded any recipes</h4>}
-      </section>
-      <section className='purchased-recipe-section'>
-      <h1>Recipes you have purchased: </h1>
-      {myPurchasedRecipes || <h4>You haven't purchased any recipes</h4>}
-      </section>
+        <header className="RecipeBook-sidebar">
+          <Link to='/profilepage'><img src={profile} alt='navigate to user profile page'/></Link>
+          <Link to='/'><img src={home} alt='navigate to main page'/></Link>
+        </header>
+        <section className='my-recipe-section'>
+          <h1>Recipes you have uploaded: </h1>
+          <Link to='/recipeform'><button>Upload another recipe</button></Link>
+          {allRecipes[0] || <h4>You haven't uploaded any recipes</h4>}
+          </section>
+          <section className='purchased-recipe-section'>
+          <h1>Recipes you have purchased: </h1>
+          {allRecipes[1] || <h4>You haven't purchased any recipes</h4>}
+        </section>
       </div>
     );
   }
