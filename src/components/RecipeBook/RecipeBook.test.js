f6b9041b8559a7dea05fdd28b0ea0fbc4c4a9745
Brigette Doelp <brigette.doelp@gmail.com>
2021-01-10 20:39:54 -0700

    Added prelim tests in comments

--- src/components/RecipeBook/RecipeBook.test.js
+++ src/components/RecipeBook/RecipeBook.test.js
@@ -2,38 +2,58 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import RecipeCard from '../RecipeCard/RecipeCard'
 import CallToAction from '../CallToAction/CallToAction.js'
+import RecipeBook from './RecipeBook';
 import { allRecipes } from '../../mockData.js'
 import { MemoryRouter } from 'react-router-dom';
 
+//RIGHT SO, the skeletons of the tests are here, but can't really be funcitonal until the
+//userProfile is a little more fleshed out. So I'm going to leave this here for when we can
+//use it. :] 
 
 //INTEGRATION TESTS
 test('uploaded recipes render', () => {
-
 
 });
 
 test('purchased recipes render', () => {
 
-
 });
 
 test('CallToAction renders if user is not logged in', () => {
-
 
 });
 
 //UNIT TESTS
 test('RecipeBook renders', () => {
+  // render(
+  //   <MemoryRouter>
+  //     <RecipeBook />
+  //   </MemoryRouter>);
+  // const uploadButton = screen.getByTestId('uploadRecipeButton')
+  // const myRecipes = screen.getByTestId('myRecipes')
+  // const purchasedRecipes = screen.getByTestId('purchasedRecipes')
+  //
+  // expect(uploadButton).toBeInTheDocument();
+  // expect(myRecipes).toBeInTheDocument();
+  // expect(purchasedRecipes).toBeInTheDocument();
+});
 
-
+test('sidebar renders', () => {
+  // render(
+  //   <MemoryRouter>
+  //     <RecipeBook />
+  //   </MemoryRouter>);
+  // const profileIcon = screen.getByTestId('profileLinkIcon')
+  // const homeIcon = screen.getByTestId('homeLinkIcon')
+  //
+  // expect(profileIcon).toBeInTheDocument();
+  // expect(homeIcon).toBeInTheDocument();
 });
 
 test('error message renders if no recipes have been uploaded', () => {
 
-
 });
 
 test('error message renders if no recipes have been purchased', () => {
-
 
 });
