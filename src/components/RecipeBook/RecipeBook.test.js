4c8f4142827e7f4d415034ab4ec0df9d2be26cfc
Brigette Doelp <brigette.doelp@gmail.com>
2021-01-10 19:32:44 -0700

    Set up base for unit and integration tests

--- src/components/RecipeBook/RecipeBook.test.js
+++ src/components/RecipeBook/RecipeBook.test.js
@@ -4,3 +4,36 @@ import RecipeCard from '../RecipeCard/RecipeCard'
 import CallToAction from '../CallToAction/CallToAction.js'
 import { allRecipes } from '../../mockData.js'
 import { MemoryRouter } from 'react-router-dom';
+
+
+//INTEGRATION TESTS
+test('uploaded recipes render', () => {
+
+
+});
+
+test('purchased recipes render', () => {
+
+
+});
+
+test('CallToAction renders if user is not logged in', () => {
+
+
+});
+
+//UNIT TESTS
+test('RecipeBook renders', () => {
+
+
+});
+
+test('error message renders if no recipes have been uploaded', () => {
+
+
+});
+
+test('error message renders if no recipes have been purchased', () => {
+
+
+});
