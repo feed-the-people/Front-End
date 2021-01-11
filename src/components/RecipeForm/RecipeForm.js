a8814986a1ad511e95572da4e82568c9f5a123c3
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-11 16:55:46 -0700

    Build out dynamic recipe form.

--- src/components/RecipeForm/RecipeForm.js
+++ src/components/RecipeForm/RecipeForm.js
@@ -1,14 +1,22 @@
 import './RecipeForm.css'
 import {Link} from 'react-router-dom'
 import { Component } from 'react';
-import { searchNonProfits } from '../../APICalls.js'
+import { searchNonProfits, createRecipe } from '../../APICalls.js'
 class RecipeForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      password: '',
+      npoName: '',
+      npoEIN: '',
+      userId: '',
+      image: '',
+      title: '',
+      description: '',
+      instructions: '',
+      ingredients: [],
       viableNPOs: [],
+      workingIngredient: '',
+      workingAmount: '',
     }
   }
   updateInput = (e) => {
@@ -21,16 +29,57 @@ class RecipeForm extends Component {
     let searchTerm = e.target.value
     let results = await searchNonProfits(searchTerm)
     console.log(results)
-    // if (results.ok) {
-    //   results.map(result => {
-    //     return <option value={result.name}> result.name </option>
-    //   })
-    // }
+    if (results.length) {
+      let options = results.map((result, index) => {
+        return <option value={result.name} key={index} id={result.ein}> {result.name}: {result.city},{result.state} </option>
+      })
+      this.setState({viableNPOs: options})
+    } else {
+
+    }
     console.log(searchTerm)
   }
 
-  submitForm = () => {
-    console.log('Submitted!')
+  chooseNPO = (e) => {
+    let name = e.target.value
+    let index = e.target.selectedIndex;
+    let ein = e.target.childNodes[index].id
+    this.setState({npoName: name, npoEIN: ein})
+  }
+
+  submitForm = async (e) => {
+    e.preventDefault()
+    let user = JSON.parse(localStorage.getItem('user'))
+    let result = await createRecipe(
+      user.id,
+      this.state.image,
+      this.state.title,
+      this.state.description,
+      this.state.instructions,
+      this.state.npoEIN,
+      this.state.npoName,
+      this.state.ingredients
+    )
+  }
+
+  buildIngredientsList = () => {
+    if (this.state.ingredients.length) {
+      let ingList = this.state.ingredients.map((ing) => {
+       return <div className='ingredientBox'>{ing.name}: {ing.amount}</div>
+      }
+     )
+    return (
+      <div>{ingList}</div>
+      )
+    }
+  }
+
+  addIngredient = (e) => {
+    e.preventDefault();
+    let ing = {name: this.state.workingIngredient, amount: this.state.workingAmount}
+    let newList = this.state.ingredients
+    newList.push(ing)
+    this.setState({ingredients: newList})
   }
 
   render() {
@@ -50,7 +99,18 @@ class RecipeForm extends Component {
           </label>
           <label>
             Ingredients
-            <input className='ingredients' type='text' onChange={this.updateInput}/>
+            {this.buildIngredientsList()}
+            <form>
+              <label>
+                Ingredient Name
+                <input className='workingIngredient' type='text' onChange={this.updateInput}/>
+              </label>
+              <label>
+                Ingredient Amount
+                <input className='workingAmount' type='text' onChange={this.updateInput}/>
+              </label>
+              <button type='submit' onClick={this.addIngredient}>Add Ingredient</button>
+            </form>
           </label>
           <label>
             Steps
@@ -65,18 +125,17 @@ class RecipeForm extends Component {
             <input className='NPO' type='text' onChange={this.searchNPOS}/>
           </label>
           <label>
-            Non-Profit Organization Options
-            <select onChange={this.chooseNPO}>
-              {this.state.viableNPOs}
-            </select>
+            Select from search results:
+            {!this.state.viableNPOs.length ? <p>No relevant matches...</p> : <select onChange={this.chooseNPO}> {this.state.viableNPOs} </select>}
           </label>
           <button type='submit' onClick={this.submitForm}> Submit My Recipe </button>
         </form>
+
         <footer className="RecipeForm-footer">
           <Link to='/'><button> Take Me Back </button></Link>
         </footer>
       </div>
-    );
+    )
   }
 }
 
