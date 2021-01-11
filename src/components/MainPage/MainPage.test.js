63252286c1c8c7b91b8d43f658727285dd5c3e0d
Brigette Doelp <brigette.doelp@gmail.com>
2021-01-10 17:06:01 -0700

    Remove unneeded import

--- src/components/MainPage/MainPage.test.js
+++ src/components/MainPage/MainPage.test.js
@@ -1,4 +1,4 @@
-import { render, screen, cleanup } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import { MemoryRouter } from 'react-router-dom'
 import MainPage from './MainPage.js';
