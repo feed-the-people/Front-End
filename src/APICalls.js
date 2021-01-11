fc15af76c4e5f4400f8a71ba62494ce7c09927f9
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-11 11:18:57 -0700

    Update api calls to return what I want/need.

--- src/APICalls.js
+++ src/APICalls.js
@@ -57,7 +57,7 @@ export const getUser = (id) => {
 }
 
 export const getUserWithRecipes = (id) => {
-  return fetch("http://localhost:8000/graphql", {
+  return fetch("https://feed-the-people-api.herokuapp.com/graphql", {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify( { query:`
@@ -90,12 +90,12 @@ export const getUserWithRecipes = (id) => {
     })
   })
   .then(response => response.json())
-  .then(response => console.log(response.data))
+  .then(response => response.data.getUser)
   .catch(error => console.log(error))
 }
 
 export const boughtRecipesByUser = (id) => {
-  return fetch("http://localhost:8000/graphql", {
+  return fetch("https://feed-the-people-api.herokuapp.com/graphql", {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify( { query:`
@@ -125,7 +125,7 @@ export const boughtRecipesByUser = (id) => {
     })
   })
   .then(response => response.json())
-  .then(response => console.log(response.data))
+  .then(response => response.data.boughtRecipesByUser)
   .catch(error => console.log(error))
 }
 
