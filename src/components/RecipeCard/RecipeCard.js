9392b3ec7ad8b788979928644e67f61e51728b67
Brigette Doelp <brigette.doelp@gmail.com>
2021-01-10 18:31:08 -0700

    Add test ids to elements for testing

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -9,11 +9,11 @@ function RecipeCard(props) {
       <section className='left-section'>
         <div className='leftHeader'>
           <h1 className='recipe-name' data-testid='recipeTitle'>{props.title}</h1>
-          <h3 className='recipe-rating'>5</h3>
+          <h3 className='recipe-rating' data-testid='recipeRating'>5</h3>
         </div>
-        <img className='recipe-image' src={allRecipes[1].image} />
+        <img className='recipe-image' data-testid='image' src={allRecipes[1].image} />
         <div className='left-footer'>
-          <h3 className='nonprofit-name'>{props.charityName}</h3>
+          <h3 className='nonprofit-name' data-testid='NPO'>{props.charityName}</h3>
           <Link to='/'><button className='purchase-button'>Give N' Get Recipe!</button></Link>
         </div>
       </section>
