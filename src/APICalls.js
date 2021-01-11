ecd88b9324c2315093caf89b03ff2ce89934ef41
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-11 14:11:03 -0700

    Update end point and results.

--- src/APICalls.js
+++ src/APICalls.js
@@ -180,7 +180,7 @@ export const recipeById = (id) => {
 }
 
 export const searchNonProfits = (searchTerm) => {
-  return fetch("http://localhost:8000/graphql", {
+  return fetch("https://feed-the-people-api.herokuapp.com/graphql", {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify( { query:`
@@ -196,7 +196,7 @@ export const searchNonProfits = (searchTerm) => {
     })
   })
   .then(response => response.json())
-  .then(response => console.log(response.data))
+  .then(response => response.data)
   .catch(error => console.log(error))
 }
 
