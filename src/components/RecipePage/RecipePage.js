c5802e206c9b2ca18644a8186d7c00129c16af79
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-12 14:31:08 -0700

    Refactor to dynamically render recipe based on url.

--- src/components/RecipePage/RecipePage.js
+++ src/components/RecipePage/RecipePage.js
@@ -1,27 +1,57 @@
 import './RecipePage.css';
+import { Component } from 'react';
+
 import {allRecipes} from '../../mockData.js'
+import { recipeById } from '../../APICalls.js'
 
-function RecipePage() {
-  let recipe1 = allRecipes[0]
-  console.log(recipe1)
-  // let recipeIngredients = recipe1.ingredients.map(ingredient => <p>{ingredient}</p>)
-
-
-  return (
-    <div className="RecipePage">
-      <header className="RecipePage-header">
+class RecipePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signedIn: false,
+      user: '',
+      allRecipes: [],
+      loading: true,
+    }
+  }
+  getRecipe = async (id) => {
+    let response = await recipeById(id)
+    let recipe = response.recipeById
+    console.log(recipe)
+    this.setState({recipe: recipe})
+  }
+  componentDidMount(){
+    this.getRecipe(this.props.id)
+  }
+  render(){
+    if (this.state.recipe){
+      return (
+        <div className="RecipePage">
+        <header className="RecipePage-header">
         <h1> Recipe Page </h1>
+        </header>
+        <section className="recipe-section">
+        <h3>{this.state.recipe.title}</h3>
+        <p><img src={this.state.recipe.image} alt='A dish of egg, bread, and other assorted garnishes' /></p>
+        <p>{this.state.recipe.description}</p>
+        <p>{this.state.recipe.instructions}</p>
+        {/* {recipeIngredients} */}
+        </section>
+        </div>
+      );
+    } else {
+      return (
+      <div className="RecipePage">
+      <header className="RecipePage-header">
+      <h1> Recipe Page </h1>
       </header>
       <section className="recipe-section">
-        <h3>{recipe1.title}</h3>
-        <p><img src={recipe1.image} alt='A dish of egg, bread, and other assorted garnishes' /></p>
-        <p>{recipe1.description}</p>
-        <p>{recipe1.instructions}</p>
-        {/* {recipeIngredients} */}
+      <h3>Loading...</h3>
       </section>
-
-    </div>
-  );
+      </div>
+    )
+    }
+  }
 }
 
 export default RecipePage;
