80b4e73cb596d9a1ba1e3cb000700b46198b4e83
Brigette Doelp <brigette.doelp@gmail.com>
2021-01-11 21:27:54 -0700

    Add unit tests to ensure component renders

--- src/components/CallToAction/CallToAction.test.js
+++ src/components/CallToAction/CallToAction.test.js
@@ -2,3 +2,32 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import CallToAction from './CallToAction.js'
 import { MemoryRouter } from 'react-router-dom';
+
+//UNIT TESTS
+test('prompt renders', () => {
+  render(
+    <MemoryRouter>
+      <CallToAction />
+    </MemoryRouter>);
+
+  const componentTitle = screen.getByTestId('componentTitle')
+  const prompt = screen.getByTestId('prompt')
+
+  expect(componentTitle).toBeInTheDocument();
+  expect(prompt).toBeInTheDocument();
+});
+
+test('nav renders', () => {
+  render(
+    <MemoryRouter>
+      <CallToAction />
+    </MemoryRouter>);
+
+  const signinButton = screen.getByTestId('signinButton')
+  const signupButton = screen.getByTestId('signupButton')
+  const homeButton = screen.getByTestId('homeButton')
+
+  expect(signinButton).toBeInTheDocument();
+  expect(signupButton).toBeInTheDocument();
+  expect(homeButton).toBeInTheDocument();
+});
