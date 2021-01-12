6d60082763746859ef7900c46a636ccc9ce906ae
Brigette Doelp <brigette.doelp@gmail.com>
2021-01-11 21:07:51 -0700

    Set up imports

--- src/components/CallToAction/CallToAction.test.js
+++ src/components/CallToAction/CallToAction.test.js
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import CallToAction from './CallToAction.js'
-import { allRecipes } from '../../mockData.js'
 import { MemoryRouter } from 'react-router-dom';
