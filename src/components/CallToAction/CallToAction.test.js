1e3064a1ec6d671f0b7554a50b3c5c5a2e92ecd0
Brigette Doelp <brigette.doelp@gmail.com>
2021-01-11 21:07:11 -0700

    Create file

---
+++ src/components/CallToAction/CallToAction.test.js
@@ -0,0 +1,5 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import CallToAction from './CallToAction.js'
+import { allRecipes } from '../../mockData.js'
+import { MemoryRouter } from 'react-router-dom';
