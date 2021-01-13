7753e03b73ee46d0ff3175870d8e6ca35baf26fa
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-12 17:16:37 -0700

    Connect modal functionality.

--- src/components/RecipePage/RecipePage.js
+++ src/components/RecipePage/RecipePage.js
@@ -1,8 +1,9 @@
 import './RecipePage.css';
 import { Component } from 'react';
 import CallToAction from '../CallToAction/CallToAction.js'
+import PurchaseModal from '../PurchaseModal/PurchaseModal.js'
 import {allRecipes} from '../../mockData.js'
-import { recipeById } from '../../APICalls.js'
+import { recipeById, boughtRecipesByUser, getAccessToRecipe } from '../../APICalls.js'
 
 class RecipePage extends Component {
   constructor(props) {
@@ -15,9 +16,25 @@ class RecipePage extends Component {
     }
   }
   getRecipe = async (id) => {
+    let user = JSON.parse(localStorage.getItem('user'))
     let response = await recipeById(id)
     let recipe = response.recipeById
     this.setState({recipe: recipe})
+    this.getPurchasedRecipes(user.id)
+  }
+  getPurchasedRecipes = async (id) => {
+    let response = await boughtRecipesByUser(id)
+    if(!response){
+      this.setState({purchased: false})
+    }
+  }
+  donate = async (e) => {
+    let user = JSON.parse(localStorage.getItem('user'))
+    e.preventDefault()
+    let amount = e.target.parentElement.firstChild.lastChild.value
+    let recipeId = this.state.recipe.id
+    let response = await getAccessToRecipe(user.id, recipeId, amount)
+    this.setState({purchased: true})
   }
   componentDidMount(){
     this.getRecipe(this.props.id)
@@ -26,7 +43,7 @@ class RecipePage extends Component {
     let user = JSON.parse(localStorage.getItem('user'))
     if (!user) {
       return <CallToAction title='You need to be signed in to view this recipe...' />
-    } else if (this.state.recipe){
+    } else if (this.state.recipe && this.state.purchased){
       return (
         <div className="RecipePage">
         <header className="RecipePage-header">
@@ -44,6 +61,7 @@ class RecipePage extends Component {
     } else {
       return (
       <div className="RecipePage">
+      {!this.state.purchased && <PurchaseModal donate={this.donate} recipe={this.state.recipe}/>}
       <header className="RecipePage-header">
       <h1> Recipe Page </h1>
       </header>
