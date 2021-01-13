28a231629d31641e17d1ab7ea7b9fe6166970082
Brigette Doelp <brigette.doelp@gmail.com>
2021-01-13 13:01:40 -0700

    Add integration test for MainPage rendering

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'
+import { MemoryRouter } from 'react-router-dom';
 
 import App from './App';
 
 test('basic title', () => {
   render(<App />);
+});
+
+test('MainPage renders' , () => {
+  render(<App />);
+
+  const profileIcon = screen.getByTestId('profileicon')
+
+  expect(profileIcon).toBeInTheDocument()
 });
