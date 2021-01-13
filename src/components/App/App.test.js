d3b9d3239107284bc97f5b3e6ef08781536a0c28
Brigette Doelp <brigette.doelp@gmail.com>
2021-01-13 13:32:17 -0700

    Add integration tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,17 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import { MemoryRouter } from 'react-router-dom';
-
 import App from './App';
 
-test('basic title', () => {
-  render(<App />);
-});
-
+//INTEGRATION TESTS
 test('MainPage renders' , () => {
   render(<App />);
 
-  const profileIcon = screen.getByTestId('profileicon')
+  const mainPage = screen.getByTestId('mainPage')
 
-  expect(profileIcon).toBeInTheDocument()
+  expect(mainPage).toBeInTheDocument()
+});
+
+test('Footer renders' , () => {
+  render(<App />);
+
+  const footer = screen.getByTestId('footer')
+
+  expect(footer).toBeInTheDocument()
 });
