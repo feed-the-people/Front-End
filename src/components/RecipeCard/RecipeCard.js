096258387ea835655750bb69b61e755888262b4a
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-12 21:53:35 -0700

    Shift recipe card layout to be more image heavy.

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -9,20 +9,19 @@ function RecipeCard(props) {
       <section className='left-section'>
         <div className='leftHeader'>
           <h1 className='recipe-name' data-testid='recipeTitle'>{props.title}</h1>
-          <h3 className='recipe-rating' data-testid='recipeRating'>5</h3>
         </div>
         <img className='recipe-image' data-testid='image' src={props.image} />
-        <div className='left-footer'>
-          <h3 className='nonprofit-name' data-testid='NPO'>{props.charityName}</h3>
-          <Link to={route}><button className='purchase-button'>Give N' Get Recipe!</button></Link>
-        </div>
       </section>
       <section className='right-section'>
         <section className='star-rating'>
         </section>
-        <p className='recipe-story' data-testid='recipeStory'>{props.description}</p>
-        <h4 className='tags' >Tags</h4>
+          <h3 className='recipe-rating' data-testid='recipeRating'>5</h3>
+        <h3 className='nonprofit-name' data-testid='NPO'>{props.charityName}</h3>
+        <Link to={route}><button className='purchase-button'>Give N' Get Recipe!</button></Link>
         <h5 className='recipe-tags' data-testid='recipeTags'></h5>
+        <div className='right-footer'>
+          <p className='recipe-story' data-testid='recipeStory'>{props.description}</p>
+        </div>
       </section>
     </div>
   );
