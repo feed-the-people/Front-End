92c417fa3a17e218583c2f0e8a7e6652ce536bde
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-22 11:00:24 -0700

    Add test file for purchase modal.

---
+++ src/components/PurchaseModal/PurchaseModal.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import PurchaseModal from './PurchaseModal.js'
+import { MemoryRouter } from 'react-router-dom';
+import { individualUserData } from '../../mockData.js';
+
+//Tests
+//Test that all the profile elements can render
+//Check that the user is logged in/Mock JSON storage
+
+test('nav renders', () => {
+
+});
+
+test('user data renders', () => {
+
+})
