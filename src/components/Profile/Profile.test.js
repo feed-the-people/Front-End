f1d3038ea2f5216835a4dcced4cd694484529b62
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-22 10:59:28 -0700

    Add profile test comments.

--- src/components/Profile/Profile.test.js
+++ src/components/Profile/Profile.test.js
@@ -4,6 +4,9 @@ import Profile from './Profile.js'
 import { MemoryRouter } from 'react-router-dom';
 import { individualUserData } from '../../mockData.js';
 
+//Tests
+//Test that all the profile elements can render
+//Check that the user is logged in/Mock JSON storage
 
 test('nav renders', () => {
 
