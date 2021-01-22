a1481aa59d8969593b1fcae72529e4809eebe544
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-22 10:49:26 -0700

    Build footer test file

---
+++ src/components/Footer/Footer.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Footer from './Footer.js'
+import { MemoryRouter } from 'react-router-dom';
+
+//UNIT TESTS
+// test that footer renders
+// check that both buttons fire when clicked
+// check that footer options dynamically render
+
+test('prompt renders', () => {
+
+});
