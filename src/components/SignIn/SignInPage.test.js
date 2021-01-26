02d385f89335be236dbf6f544daeaa9fbe5495bb
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-26 14:28:29 -0700

    Write user event sign in tests.

--- src/components/SignIn/SignInPage.test.js
+++ src/components/SignIn/SignInPage.test.js
@@ -2,16 +2,71 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import SignInPage from './SignInPage';
 import { MemoryRouter } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
+import { userSignIn } from '../../APICalls.js'
+jest.mock('../../apiCalls.js')
 
-//Test that sign in loads
-//Test that sad path happens
-//Test that mocked resonses will render different options
-test('basic title', () => {
-  render(
-    <MemoryRouter>
+describe('SignInPage', () => {
+  it('should render basic form elements', () => {
+    render(
+      <MemoryRouter>
       <SignInPage />
-    </MemoryRouter>
-  );
-  const title = screen.getByText("Welcome Back! Sign In Below:");
-  expect(title).toBeInTheDocument();
-});
+      </MemoryRouter>
+    );
+    const title = screen.getByTestId("sign-in-title");
+    const username = screen.getByLabelText("Username");
+    const password = screen.getByLabelText("Password");
+    const submit = screen.getByTestId("submit-button");
+    expect(title).toBeInTheDocument();
+    expect(username).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+    expect(submit).toBeInTheDocument();
+  });
+  it('should render basic form elements', () => {
+    render(
+      <MemoryRouter>
+      <SignInPage />
+      </MemoryRouter>
+    );
+    const submit = screen.getByTestId("submit-button");
+    expect(submit).toBeInTheDocument();
+    expect(submit).toBeDisabled();
+  });
+  it('should render basic form elements', () => {
+    render(
+      <MemoryRouter>
+      <SignInPage />
+      </MemoryRouter>
+    );
+    const title = screen.getByTestId("sign-in-title");
+    const username = screen.getByLabelText("Username");
+    const password = screen.getByLabelText("Password");
+    const submit = screen.getByTestId("submit-button");
+    userEvent.type(username, '12345')
+    userEvent.type(password, '12345')
+    expect(submit).toBeEnabled();
+  });
+  it('should render basic form elements', () => {
+    render(
+      <MemoryRouter>
+      <SignInPage />
+      </MemoryRouter>
+    );
+    const title = screen.getByTestId("sign-in-title");
+    const username = screen.getByLabelText("Username");
+    const password = screen.getByLabelText("Password");
+    const submit = screen.getByTestId("submit-button");
+    userEvent.type(username, 'nathanielmillard')
+    userEvent.type(password, '12345')
+    expect(submit).toBeEnabled();
+    userEvent.click(submit)
+    expect(userSignIn).toHaveBeenCalledTimes(1)
+    expect(userSignIn).toHaveBeenCalledWith('nathanielmillard', '12345')
+  });
+})
+
+//Stretch Testing:
+//Write and Test that there is sad path handling for:
+  //username to not have spaces
+  //password
+  //rejections to sign in
