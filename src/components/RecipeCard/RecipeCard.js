25daa194806928ea2c314aaa5d42a31ee3ebd4ca
Nathaniel Millard <nathanielmillard@gmail.com>
2021-01-29 22:00:24 -0700

    Restyle recipe cards.

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -2,14 +2,14 @@ import './RecipeCard.scss';
 import {Link} from 'react-router-dom'
 import { getUser } from '../../APICalls.js'
 import { Component, Redirect } from 'react';
+import StarRatingComponent from 'react-star-rating-component';
 
  let RecipeCard = (props) => {
   //This whole card needs a thorough comb over and I think we could be performing
   //way less logic and actually just look at dynamic props
-
   let route = `/recipepage/${props.id}`
   let user = JSON.parse(localStorage.getItem('user'));
-  let buttonText, rating;
+  let buttonText;
 
   if(user && user.userRecipes.find(recipe => recipe.recipeId == props.id)){
     buttonText = 'Already Donated'
@@ -18,35 +18,43 @@ import { Component, Redirect } from 'react';
   } else {
     buttonText = "Give N' Get Recipe"
   }
-
-  if (props.rating !== null) {
-    rating = props.rating + " out of 5";
+  let charityName
+  if (props.charityName.split(' ').length > 1) {
+    charityName = props.charityName.toLowerCase()
+    charityName = charityName.split(' ')
+    charityName = charityName.map(word => {
+      let letters = word.split('')
+      letters[0] = letters[0].toUpperCase();
+      return letters.join('')
+    })
+    charityName = charityName.join(' ')
   } else {
-    rating = "Not yet rated";
+    charityName = props.charityName
   }
 
   return (
-    <div className="RecipeCard">
-      <section className='left-section'>
-        <div className='leftHeader'>
-          <h1 className='recipe-name' data-testid='recipeTitle'>{props.title}</h1>
-        </div>
-        <img className='recipe-image' data-testid='image' src={props.image} />
+    <section className="RecipeCard">
+      <header className='RecipeCard-header'>
+        <h1 className='RecipeCard-name' data-testid='recipeTitle'>{props.title}</h1>
+        <StarRatingComponent
+        className='RecipeCard-rating'
+        name={'average-rating'} /* name of the radio input, it is required */
+        value={5} /* number of selected icon (`0` - none, `1` - first) */
+        starCount={props.rating} /* number of icons in rating, default `5` */
+        starColor={'#f4c144'} /* color of selected icons, default `#ffb400` */
+        emptyStarColor={'#fefaec'} /* color of non-selected icons, default `#333` */
+        editing={false} /* is component available for editing, default `true` */
+        />
+      </header>
+      <section className='bottom-section'>
+        <Link to={route} className='image-link'>
+          <img className='RecipeCard-image' data-testid='image' src={props.image} />
+        </Link>
+        <h3 className='nonprofit-name' data-testid='NPO'>{'Donations go to: ' + charityName}</h3>
+        <p className='RecipeCard-story' data-testid='recipeStory'>{props.description}</p>
+        <Link to={route} className='purchase-button'>{buttonText}</Link>
       </section>
-      <section className='right-section'>
-        <section className='star-rating'>
-          <h3 className='recipe-rating' data-testid='recipeRating'>{rating}</h3>
-        </section>
-        <section className='details'>
-          <p className='recipe-story' data-testid='recipeStory'>{props.description}</p>
-          <p className='donation-blurb' >Donations go to: </p>
-        </section>
-        <h3 className='nonprofit-name' data-testid='NPO'>{props.charityName}</h3>
-        <div className='right-footer'>
-          <Link to={route} className='purchase-button'>{buttonText}</Link>
-        </div>
-      </section>
-    </div>
+    </section>
   );
 }
 
